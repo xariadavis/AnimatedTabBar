caad6dc40c1d9f8462a0219d32fdb3cd9cf55766
xariadavis <55540782+xariadavis@users.noreply.github.com>
2024-02-11 13:19:58 -0500

    Create Colors.swift

---
+++ Sources/AnimatedTabBar/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  
+//
+//  Created by Xaria Davis on 2/11/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let defaultMainColor = Color(red: 0.925, green: 0.941, blue: 0.953)
+    static let darkDefaultMainColor = Color(red: 0.188, green: 0.192, blue: 0.208)
+}
